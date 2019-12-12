54888ffb4da6da40d1209946b2538295a6a7b797
wangzilai <980697169@qq.com>
2019-12-12 14:37:48 +0800

    second version

--- products.py
+++ products.py
@@ -1,8 +1,12 @@
-products=[]
+products = []
 while True:
-    name=input('enter the name of the product:')
-    if name=='q':
+    name = input('enter the name of the product:')
+    if name == 'q':
         break
-    price=input('enter the price of the product:')
-    products.append([name.strip(),price.strip()])
+    price = input('enter the price of the product:')
+    products.append([name.strip(), price.strip()])
 print(products)
+with open('products.csv', 'w',encoding='utf-8')as f:
+    f.write('商品,价格\n')
+    for p in products:
+        f.write(p[0] + ',' + p[1] + '\n')
