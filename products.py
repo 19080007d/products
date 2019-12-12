77d56f8b50664d9d76bc28d8adf468ae45b05213
wangzilai <980697169@qq.com>
2019-12-12 15:07:51 +0800

    read_file and encoding

--- products.py
+++ products.py
@@ -1,4 +1,11 @@
 products = []
+with open('products.csv', 'r', encoding='utf-8')as f:
+    for line in f:
+        if '商品,价格' in line:
+            continue
+        name, price = line.strip().split(',')
+        products.append([name, price])
+print(products)
 while True:
     name = input('enter the name of the product:')
     if name == 'q':
@@ -6,7 +13,7 @@ while True:
     price = input('enter the price of the product:')
     products.append([name.strip(), price.strip()])
 print(products)
-with open('products.csv', 'w',encoding='utf-8')as f:
+with open('products.csv', 'w', encoding='utf-8')as f:
     f.write('商品,价格\n')
     for p in products:
         f.write(p[0] + ',' + p[1] + '\n')
