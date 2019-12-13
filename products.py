e55713a23154c656089ed412a676ed3853dda6fa
wangzilai <980697169@qq.com>
2019-12-13 15:43:17 +0800

    refactor

--- products.py
+++ products.py
@@ -1,32 +1,50 @@
 # 检查档案的有无
 import os
 
-products = []
-if os.path.isfile('products.csv'):
-    print('find it')
-    # 读取档案
-    with open('products.csv', 'r', encoding='utf-8')as f:
-        for line in f:
-            if '商品,价格' in line:
-                continue
-            name, price = line.strip().split(',')
-            products.append([name, price])
-    print(products)
 
-else:
-    print('not find the file')
+def read_file(filename):
+    products = []
+    if os.path.isfile(filename):
+        print('find it')
+        # 读取档案
+        with open(filename, 'r', encoding='utf-8')as f:
+            for line in f:
+                if '商品,价格' in line:
+                    continue
+                name, price = line.strip().split(',')
+                products.append([name, price])
+
+    else:
+        print('not find the file')
+    return products
 
 
-# 补充档案
-while True:
-    name = input('enter the name of the product:')
-    if name == 'q':
-        break
-    price = input('enter the price of the product:')
-    products.append([name.strip(), price.strip()])
-print(products)
-# 写档案
-with open('products.csv', 'w', encoding='utf-8')as f:
-    f.write('商品,价格\n')
+def print_file(products):
     for p in products:
-        f.write(p[0] + ',' + p[1] + '\n')
+        print(p[0], '的价格是', p[1])
+
+
+def user_input(products):
+    # 补充档案
+    while True:
+        name = input('enter the name of the product:')
+        if name == 'q':
+            break
+        price = input('enter the price of the product:')
+        products.append([name.strip(), price.strip()])
+    print(products)
+    return products
+
+
+def write_file(filename):
+    # 写档案
+    with open(filename, 'w', encoding='utf-8')as f:
+        f.write('商品,价格\n')
+        for p in products:
+            f.write(p[0] + ',' + p[1] + '\n')
+
+
+products = read_file('products.csv')
+print_file(products)
+products = user_input(products)
+write_file('products.csv')
